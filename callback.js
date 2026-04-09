9582fa0ba705d0ee3fc1e8247543a82e3b392c0e
003-demo <rrriku05@gmail.com>
2026-04-09 22:23:10 +0600

    callback

--- callback.js
+++ callback.js
@@ -1,1 +1,9 @@
-console.log("callback")
+function greet(name,callback){
+    console.log("Hello " + name);
+    callback()
+}
+
+function saybye(){
+    console.log("Goodbye!")
+}
+greet("Rohan",saybye)
